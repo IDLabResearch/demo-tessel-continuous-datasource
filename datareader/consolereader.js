1e48a1df6cfc9d1ce69252ece53553d00f759277
Ruben Taelman <ruben.taelman@ugent.be>
2016-06-17 11:43:16 +0200

    Emit read objects on one line

--- datareader/consolereader.js
+++ datareader/consolereader.js
@@ -1,3 +1,5 @@
 var reader = require('./reader.js');
-reader.emit(parseInt(process.argv[2]), 'c', console.log);
+reader.emit(parseInt(process.argv[2]), 'c', function(data) {
+    console.log(JSON.stringify(data));
+});
 
