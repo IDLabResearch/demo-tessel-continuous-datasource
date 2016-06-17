7905173d46a3f56f074186002ee4a1f9b35b741f
Ruben Taelman <ruben.taelman@ugent.be>
2016-06-17 16:04:47 +0200

    Remove .properties from repo

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Starts the complete pipeline
 
+if [ ! -f .properties ]; then
+    echo "No .properties has been found! Create one from the template."
+    exit 1
+fi
+
 # Start live LDF server
 datasourceinserter/startserver.sh &
 
