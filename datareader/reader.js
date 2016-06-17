286d863ebf59b75aa510dc5bf545e85cea785179
Ruben Taelman <ruben.taelman@ugent.be>
2016-06-17 09:12:12 +0200

    Add reader module

---
+++ datareader/reader.js
@@ -0,0 +1,31 @@
+var tessel = require('tessel');
+var climatelib = require('climate-si7020');
+var climate = climatelib.use(tessel.port['A']);
+
+function emit(timeout, temperatureEncoding, cb) {
+    climate.on('ready', function() {
+        setImmediate(function loop() {
+            climate.readTemperature(temperatureEncoding, function (errTemp, temp) {
+                climate.readHumidity(function (errHumid, humid) {
+                    cb({
+                        module: 'climate',
+                        temperature: {
+                            value: temp,
+                            error: errTemp
+                        },
+                        humidity: {
+                            value: humid,
+                            error: errHumid
+                        }
+                    });
+                    setTimeout(loop, timeout);
+                });
+            });
+        });
+    });
+}
+
+module.exports = {
+  emit: emit
+};
+
