37a4f4411a152004c973d97cdad57638fea09a1d
Ruben Taelman <ruben.taelman@ugent.be>
2016-06-17 11:20:45 +0200

    Add .properties file to define interval size

--- datareader/read.sh
+++ datareader/read.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Emits a json object with temperature and humidity every five seconds
-tessel run consolereader.js 5000 2> /dev/null
+. ../.properties
+tessel run consolereader.js $intervalsize 2> /dev/null
 
