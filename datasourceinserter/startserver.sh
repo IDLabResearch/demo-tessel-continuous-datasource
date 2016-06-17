a6967d468b201c0560231498c8f071acb4eb6a18
Ruben Taelman <ruben.taelman@ugent.be>
2016-06-17 11:36:33 +0200

    Fix startserver not finding bin

--- datasourceinserter/startserver.sh
+++ datasourceinserter/startserver.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Starts an LDF-server that supports data insertion on a different port.
+
 export TYPE="graphs"
 export SERVER="$(pwd)/node_modules/ldf-server/bin/ldf-server"
 
-cd $QS_LOCATION/bin
+cd node_modules/tpf-querystreamer/bin
 node ldf-server-http-inserter config_citybench.json 3000 1
 
